9ac083d6197a04e3b5cd1ada3cdc4fa16afd4afc
Kushagra Savya Choudhary <116177190+kushagrasavya@users.noreply.github.com>
2025-06-28 13:35:34 +0530

    Delete database.py

--- database.py
+++
@@ -1,9 +0,0 @@
-from motor.motor_asyncio import AsyncIOMotorClient
-
-client = AsyncIOMotorClient("mongodb://localhost:27017")
-db = client.spatial_data
-
-# Ensure indexes
-async def setup_indexes():
-    await db.points.create_index([("location", "2dsphere")])
-    await db.polygons.create_index([("area", "2dsphere")])
